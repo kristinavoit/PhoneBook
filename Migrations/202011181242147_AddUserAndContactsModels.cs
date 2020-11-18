2112d7badaf51c18204dca2e51ef191d974a1dee
Kristina <headwayup9@gmail.com>
2020-11-18 14:45:08 +0200

    add migration of users and contacts models

---
+++ Migrations/202011181242147_AddUserAndContactsModels.cs
@@ -0,0 +1,38 @@
+﻿namespace PhonesBook.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddUserAndContactsModels : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Contacts",
+                c => new
+                    {
+                        contactID = c.Int(nullable: false, identity: true),
+                        contactName = c.String(),
+                        phoneNumber = c.String(),
+                    })
+                .PrimaryKey(t => t.contactID);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        userID = c.Int(nullable: false, identity: true),
+                        userName = c.String(),
+                        isAdmin = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.userID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+            DropTable("dbo.Contacts");
+        }
+    }
+}
---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace PhonesBook.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<PhonesBook.UserContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(PhonesBook.UserContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
